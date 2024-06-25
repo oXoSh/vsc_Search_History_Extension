cf87e1aa518e02099a24e30b40b64e88b0a23ede
shioih <1554474779@qq.com>
2024-06-25 23:13:52 +0800

    complete refresh function

--- src/searchHistoryView.ts
+++ src/searchHistoryView.ts
@@ -4,11 +4,14 @@ import path from 'path';
 
 export class SearchHistoryProvider implements vscode.TreeDataProvider<string> {
 
+    private _onDidChangeTreeData: vscode.EventEmitter<string | undefined> = new vscode.EventEmitter<string | undefined>();
+    readonly onDidChangeTreeData: vscode.Event<string | undefined> = this._onDidChangeTreeData.event;
+
     constructor(private context: vscode.ExtensionContext) {
     }
 
     refresh(): any {
-        // this._onDidChangeTreeData.fire(undefined);
+        this._onDidChangeTreeData.fire(undefined);
     }
 
     async getChildren(key: string | undefined): Promise<string[]> {
